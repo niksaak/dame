c7bdf8b05f74876be43e5d0d42dbf7559477b9a8
NikSaak <niksaak@gmail.com>
2013-02-20 18:55:49 +0200

    removed unnecessary files

--- src/entities/particle.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "entity.h"
-
-extern Entity* emit_particle(cpSpace* space, cpBody* body,
-    cpVect offset, cpVect impulse);
--- src/states.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "engine/stateman.h"
-
-extern state_t state_main();
