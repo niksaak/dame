f4554a2119b67f16c84b391d3294d7e036057dfa
Mikola Samardak <niksaak@gmail.com>
2013-08-26 18:03:59 +0300

    fix DEFTEST, no crashing when Tester is NULL, make definitions prettier

--- test/test.h
+++ test/test.h
@@ -18,40 +18,40 @@ typedef struct Tester {
 
   const char* test;         // name of the current test
   const char* assertion;    // name of the current assertion
-  bool fail;                // true if the test is failed
+  bool fail;                // true if the test has failed
 
   int testc;                // test count
   int passc;                // passed test count
   int failc;                // failed test count
 } Tester;
 
-#define START_DEFTEST(test_name)  \
-  void test_name(Tester* _tester) { \
-    if(_tester == NULL) { \
-      fprintf(stderr, "Fatal error: tester is NULL\n"); \
-      exit(EXIT_FAILURE); \
-    } \
-    _tester->test = __func__; \
-    _tester->assertion = NULL;  \
-    _tester->fail = false;  \
-    _tester->testc++;
+#define START_DEFTEST(test_name)                        \
+  static void test_name(Tester* _tester) {              \
+    _tester->test = __func__;                           \
+    _tester->testc++;                                   \
+    if(_tester == NULL) {                               \
+      _tester->assertion = "Tester is not NULL";        \
+      _tester->fail = false;                            \
+    }                                                   \
+    _tester->assertion = NULL;                          \
+    _tester->fail = false;
 
-#define END_DEFTEST \
+#define END_DEFTEST     \
     if(_tester->fail) { \
       _tester->failc++; \
-    } else {  \
+    } else {            \
       _tester->passc++; \
-    } \
+    }                   \
   }
 
-#define TASSERT(a)  \
-  do {  \
-    _tester->assertion = #a;  \
-    if(!(a)) {  \
-      _tester->fail = true; \
+#define TASSERT(a)                                    \
+  do {                                                \
+    _tester->assertion = #a;                          \
+    if(!(a)) {                                        \
+      _tester->fail = true;                           \
       fprintf(stderr, "%s: assertion (%s) failed\n",  \
-              _tester->name, _tester->assertion); \
-    } \
+              _tester->name, _tester->assertion);     \
+    }                                                 \
   } while(0)
 
 #define TEST(name, tester) name(&tester);
