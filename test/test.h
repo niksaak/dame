a47ac99364abe8615824d4507ed9f692b3753367
Mikola Samardak <niksaak@gmail.com>
2013-08-27 02:51:08 +0300

    upgrade test framework
    + added no-fallthrough assertion macro
    + fixed DEFTESTs
    + TEST() is now generic
    
    As a side note, DEFTEST macros now use goto. Scary-scary~
    
    See #6, #4.

--- test/test.h
+++ test/test.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,13 +10,14 @@
  * struct Tester - structure with data necessary for performing tests
  * START_DEFTEST(name) - start test definition
  * END_DEFTEST - end test definition
- * TASSERT(assertion) - check assertion
+ * TASSERT(assertion) - fallthrough assert
+ * TEASSERT(assertion) - no fallthrough assert
  * TEST(name, tester) - run test named name with tester
  * print_test_summary(runner) - print summary for tests ran with runner
  */
 
 typedef struct Tester {
-  const char* name;  // name for runner
+  const char* name;         // name for runner
 
   const char* test;         // name of the current test
   const char* assertion;    // name of the current assertion
@@ -25,18 +28,17 @@ typedef struct Tester {
   int failc;                // failed test count
 } Tester;
 
-#define START_DEFTEST(test_name)                        \
-  static void test_name(Tester* _tester) {              \
-    _tester->test = __func__;                           \
-    _tester->testc++;                                   \
-    if(_tester == NULL) {                               \
-      _tester->assertion = "Tester is not NULL";        \
-      _tester->fail = false;                            \
-    }                                                   \
-    _tester->assertion = NULL;                          \
+#define START_DEFTEST(test_name)           \
+  static void test_name(Tester* _tester)   \
+  {                                        \
+    _tester->test = __func__;              \
+    _tester->testc++;                      \
+    TEASSERT(_tester != NULL);             \
+    _tester->assertion = NULL;             \
     _tester->fail = false;
 
 #define END_DEFTEST     \
+  ret:                  \
     if(_tester->fail) { \
       _tester->failc++; \
     } else {            \
@@ -54,7 +56,21 @@ typedef struct Tester {
     }                                                 \
   } while(0)
 
-#define TEST(name, tester) name(&tester);
+#define TEASSERT(a)                                   \
+  do {                                                \
+    _tester->assertion = #a;                          \
+    if(!(a)) {                                        \
+      fprintf(stderr, "%s: assertion (%s) failed\n",  \
+              _tester->name, _tester->assertion);     \
+      goto ret;                                       \
+    }                                                 \
+  } while(0)
+
+#define TEST(name, tester)    \
+  name(_Generic((tester),     \
+           Tester: &(tester), \
+           Tester*: (tester), \
+           void*: (tester)))
 
 static inline void print_test_summary(const Tester r)
 {
