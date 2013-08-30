fc48c18947ffecd560d8a2775ee1018690de006b
Mikola Samardak <niksaak@gmail.com>
2013-08-31 00:33:19 +0300

    sketch out basic interface for entity manager
    See #9.

---
+++ src/entity.c
@@ -0,0 +1,36 @@
+#include "entity.h"
+
+entity_t mkentity_kind(void* obj, entity_kind_t kind)
+{
+  return -1; // TODO
+}
+
+/* Remove Entity */
+
+int kmentity(entity_t id)
+{
+  return -1; // TODO
+}
+
+/* Get Entity */
+
+entity_kind_t entity_kind(entity_t id)
+{
+  return -1; // TODO
+}
+
+void* entity(entity_t id)
+{
+  return NULL; // TODO
+}
+
+void* sentity(entity_t id, entity_kind_t kind)
+{
+  return NULL; // TODO
+}
+
+const char* entity_kind_name(entity_kind_t kind)
+{
+  return NULL; // TODO
+}
+
