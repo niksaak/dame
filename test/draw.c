7afafb6cbcad35233f9aa1c7727a8322f3089a4b
Mikola Samardak <niksaak@gmail.com>
2013-09-07 18:20:23 +0300

    move gfx tests to draw suite
    See #12.

--- test/draw.c
+++ test/draw.c
@@ -2,6 +2,39 @@
 #include "../src/draw.h"
 #include "../src/sys.h"
 
+/* gfx */
+
+START_TEST(start_gfx_zero_dimensions_returns_nonzero)
+{
+  ck_assert_int_ne(start_gfx("test", 0, 0), 0);
+  stop_gfx();
+}
+END_TEST
+
+START_TEST(start_gfx_title_null_sets_default_title)
+{
+  ck_abort_msg("TODO"); // TODO
+}
+END_TEST
+
+START_TEST(setzoom_returns_previous_zoom)
+{
+  double z0;
+  double z1 = 0.5;
+  double z2 = 1;
+  double z3;
+
+  z0 = setzoom(z1);
+  z3 = setzoom(z2);
+
+  ck_assert_msg(z3 == z1, "z3 == %g", z3);
+
+  setzoom(z0);
+}
+END_TEST
+
+/* draw-NULL */
+
 static void setup(void)
 {
   start_gfx("draw_null_test", 128, 128);
@@ -39,14 +72,23 @@ END_TEST
 Suite* mk_draw_suite(void)
 {
   Suite* s = suite_create("draw");
-  TCase* c = tcase_create("draw-NULL");
+  TCase* tgfx = tcase_create("gfx");
+  TCase* tdnu = tcase_create("draw-NULL");
 
-  tcase_add_checked_fixture(c, setup, teardown);
-  tcase_add_test(c, draw_points_null_returns_nonzero);
-  tcase_add_test(c, draw_polyline_null_returns_nonzero);
-  tcase_add_test(c, draw_polygon_null_returns_nonzero);
-  tcase_add_test(c, draw_curve_null_returns_nonzero);
-  suite_add_tcase(s, c);
+  // gfx:
+  tcase_add_test(tgfx, start_gfx_zero_dimensions_returns_nonzero);
+  tcase_add_test(tgfx, start_gfx_title_null_sets_default_title);
+  tcase_add_test(tgfx, setzoom_returns_previous_zoom);
+
+  // draw-NULL:
+  tcase_add_checked_fixture(tdnu, setup, teardown);
+  tcase_add_test(tdnu, draw_points_null_returns_nonzero);
+  tcase_add_test(tdnu, draw_polyline_null_returns_nonzero);
+  tcase_add_test(tdnu, draw_polygon_null_returns_nonzero);
+  tcase_add_test(tdnu, draw_curve_null_returns_nonzero);
+
+  suite_add_tcase(s, tgfx);
+  suite_add_tcase(s, tdnu);
 
   return s;
 }
--- test/sys.c
+++ test/sys.c
@@ -1,36 +1,7 @@
 #include <check.h>
 #include "../src/sys.h"
 
-/* TODO: move gfx tests to draw
-START_TEST(start_gfx_zero_dimensions_returns_nonzero)
-{
-  ck_assert_int_ne(start_gfx("test", 0, 0), 0);
-  stop_gfx();
-}
-END_TEST
-
-START_TEST(start_gfx_title_null_sets_default_title)
-{
-  ck_abort_msg("TODO"); // TODO
-}
-END_TEST
-
-START_TEST(setzoom_returns_previous_zoom)
-{
-  double z0;
-  double z1 = 0.5;
-  double z2 = 1;
-  double z3;
-
-  z0 = setzoom(z1);
-  z3 = setzoom(z2);
-
-  ck_assert_msg(z3 == z1, "z3 == %g", z3);
-
-  setzoom(z0);
-}
-END_TEST
-*/
+/* space */
 
 START_TEST(init_space_returns_current_space)
 {
@@ -71,18 +42,12 @@ END_TEST
 Suite* mk_sys_suite(void)
 {
   Suite* s = suite_create("sys");
-  // TCase* tgfx = tcase_create("gfx"); TODO
   TCase* tspace = tcase_create("space");
 
-  /*
-  tcase_add_test(tgfx, start_gfx_zero_dimensions_returns_nonzero);
-  tcase_add_test(tgfx, start_gfx_title_null_sets_default_title);
-  tcase_add_test(tgfx, setzoom_returns_previous_zoom);
-  */
   tcase_add_test(tspace, init_space_returns_current_space);
   tcase_add_test(tspace, remove_body_works);
   tcase_add_test(tspace, remove_body_null_returns_nonzero);
-  // suite_add_tcase(s, tgfx);
+
   suite_add_tcase(s, tspace);
 
   return s;
