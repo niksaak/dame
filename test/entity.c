483558ff9309968fa241562a7cb40218fddd07e0
Mikola Samardak <niksaak@gmail.com>
2013-09-08 00:59:29 +0300

    move physics tests to entity suite
    See #12.

--- test/entity.c
+++ test/entity.c
@@ -3,6 +3,8 @@
 #include "../src/module.h"
 #include "../src/sys.h"
 
+/* entities */
+
 START_TEST(mkentity_null_returns_bad_entity)
 {
   ck_assert_int_eq(mkentity((module_t*)NULL), BAD_ENTITY);
@@ -122,25 +124,70 @@ START_TEST(sentity_nil_returns_null)
 }
 END_TEST
 
+/* space */
+
+START_TEST(init_space_returns_current_space)
+{
+  cpSpace* s = init_space();
+
+  ck_assert_ptr_ne(s, NULL);
+  ck_assert_ptr_ne(current_space, NULL);
+  ck_assert_ptr_eq(s, current_space());
+
+  deinit_space();
+}
+END_TEST
+
+START_TEST(remove_body_works)
+{
+  cpSpace* s = init_space();
+  cpBody* b = cpSpaceAddBody(s, cpBodyNew(1, 1));
+  cpShape* sh = cpSpaceAddShape(s, cpCircleShapeNew(b, 1, cpvzero));
+  remove_body(b);
+
+  ck_assert_ptr_ne(s, NULL);
+  ck_assert_ptr_ne(b, NULL);
+  ck_assert_ptr_ne(sh, NULL);
+
+  ck_assert(!cpSpaceContainsBody(s, b));
+  ck_assert(!cpSpaceContainsShape(s, sh));
+
+  deinit_space();
+}
+END_TEST
+
+START_TEST(remove_body_null_returns_nonzero)
+{
+  ck_assert_int_ne(remove_body(NULL), 0);
+}
+END_TEST
+
 Suite* mk_entity_suite(void)
 {
   Suite* s = suite_create("entity");
-  TCase* c = tcase_create("core");
+  TCase* tent = tcase_create("core");
+  TCase* tspc = tcase_create("space");
 
-  tcase_add_test(c, mkentity_null_returns_bad_entity);
-  tcase_add_test(c, mkentity_works);
-  tcase_add_test(c, kmentity_bad_entity_returns_nonzero);
-  tcase_add_test(c, kmentity_nil_returns_nonzero);
-  tcase_add_test(c, kmentity_works);
-  tcase_add_test(c, entity_kind_works);
-  tcase_add_test(c, entity_kind_bad_entity_returns_bad_entity);
-  tcase_add_test(c, entity_kind_nil_returns_bad_entity);
-  tcase_add_test(c, entity_bad_entity_returs_null);
-  tcase_add_test(c, entity_nonexistant_returns_null);
-  tcase_add_test(c, sentity_works);
-  tcase_add_test(c, sentity_bad_entity_returns_null);
-  tcase_add_test(c, sentity_nil_returns_null);
-  suite_add_tcase(s, c);
+  tcase_add_test(tent, mkentity_null_returns_bad_entity);
+  tcase_add_test(tent, mkentity_works);
+  tcase_add_test(tent, kmentity_bad_entity_returns_nonzero);
+  tcase_add_test(tent, kmentity_nil_returns_nonzero);
+  tcase_add_test(tent, kmentity_works);
+  tcase_add_test(tent, entity_kind_works);
+  tcase_add_test(tent, entity_kind_bad_entity_returns_bad_entity);
+  tcase_add_test(tent, entity_kind_nil_returns_bad_entity);
+  tcase_add_test(tent, entity_bad_entity_returs_null);
+  tcase_add_test(tent, entity_nonexistant_returns_null);
+  tcase_add_test(tent, sentity_works);
+  tcase_add_test(tent, sentity_bad_entity_returns_null);
+  tcase_add_test(tent, sentity_nil_returns_null);
+
+  tcase_add_test(tspc, init_space_returns_current_space);
+  tcase_add_test(tspc, remove_body_works);
+  tcase_add_test(tspc, remove_body_null_returns_nonzero);
+
+  suite_add_tcase(s, tent);
+  suite_add_tcase(s, tspc);
 
   return s;
 }
