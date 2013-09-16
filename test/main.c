dbba7ae2e203bc396f13ad3d0f398254442bf7e9
Mikola Samardak <niksaak@gmail.com>
2013-09-16 03:32:05 +0300

    add error unittest

--- test/main.c
+++ test/main.c
@@ -2,9 +2,10 @@
 #include <check.h>
 
 Suite* mk_draw_suite(void);
+Suite* mk_entity_suite(void);
+Suite* mk_error_suite(void);
 Suite* mk_module_suite(void);
 Suite* mk_particle_suite(void);
-Suite* mk_entity_suite(void);
 
 int main(void)
 {
@@ -13,6 +14,7 @@ int main(void)
   srunner_add_suite(r, mk_particle_suite());
   srunner_add_suite(r, mk_module_suite());
   srunner_add_suite(r, mk_entity_suite());
+  srunner_add_suite(r, mk_error_suite());
 
   srunner_run_all(r, CK_ENV);
   nf = srunner_ntests_failed(r);
